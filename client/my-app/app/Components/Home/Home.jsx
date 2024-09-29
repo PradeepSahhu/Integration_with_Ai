80a58e077822fa8363b2c172d16f8e3a296d2d59
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-09-30 01:29:55 +0530

    added

--- client/my-app/app/Components/Home/Home.jsx
+++ client/my-app/app/Components/Home/Home.jsx
@@ -45,39 +45,47 @@ export default function HomePage() {
     <>
       <div>
         <div>
-          <form className="text-white m-10 block">
+          <form className="text-white m-10  flex flex-col gap-y-[1rem]">
             <label className="mr-5">Name</label>
             <input
               name="name"
               onChange={handleChange}
-              className="rounded-xl px-3 py-2 text-black"
+              className="rounded-xl px-3 py-2 text-black bg-slate-500 focus:ouline-none border-none focus:outline-black w-1/2"
             />
             <label className="mr-5">UID</label>
             <input
               onChange={handleChange}
               name="uid"
-              className="rounded-xl px-3 py-2 text-black"
+              className="rounded-xl px-3 py-2 text-black bg-slate-500 w-1/2"
             />
             <label className="mr-5">Section</label>
             <input
               onChange={handleChange}
               name="section"
-              className="rounded-xl px-3 py-2 text-black"
+              className="rounded-xl px-3 py-2 text-black bg-slate-500 w-1/2"
             />
           </form>
-          <button
-            className="px-3 py-2 bg-yellow-400 text-black rounded-xl"
-            onClick={flask_data}
-          >
-            Submit Form
-          </button>
-          <button
-            className="px-3 py-2 bg-yellow-400 text-black rounded-xl ml-10"
-            onClick={sendToBackend}
-          >
-            send data
-          </button>
-          <Link href={`/signup`}>Navigate to Other</Link>
+
+          <div className="flex gap-x-[10rem] justify-center">
+            <button
+              className="px-3 py-2 bg-yellow-400 text-black rounded-xl"
+              onClick={flask_data}
+            >
+              Submit Form
+            </button>
+            <button
+              className="px-3 py-2 bg-yellow-400 text-black rounded-xl ml-10"
+              onClick={sendToBackend}
+            >
+              send data
+            </button>
+            {/* <Link
+              className="px-4 py-2 bg-yellow-400 rounded-xl text-black"
+              href={`/signup`}
+            >
+              Navigate to Other
+            </Link> */}
+          </div>
         </div>
       </div>
     </>
